4b4be438d41679d9fff457013041657af57f8dc1
Rodrigo-Bao <123023817+Rodrigo-Bao@users.noreply.github.com>
2023-03-11 23:10:57 -0300

    Projeto de estudo The Last Of Us
    Projeto teste para estudo.

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,54 @@
-const botaoTrailer = document.querySelector(".botao-trailer");
-const botaoFecharModal = document.querySelector(".fechar-modal");
-const video = document.getElementById("video");
-const modal = document.querySelector(".modal");
-const linkDoVideo = video.src;
+/* 
+	OBJETIVO - quando clicarmos no botão temos que mostrar a imagem de fundo correspondente
 
-function alternarModal(){
-    modal.classList.toggle("aberto");
+    - passo 1 - dar um jeito de pegar o elemento HTML dos botões
+
+	- passo 2 - dar um jeito de identificar o clique do usuário no botão
+
+	- passo 3 - desmarcar o botão selecionado anterior
+
+	- passo 4 - marcar o botão clicado como se estivesse selecionada
+
+	- passo 5 - esconder a imagem de fundo anterior
+
+	- passo 6 - fazer aparecer a imagem de fundo correspondente ao botão clicado
+*/
+
+// passo 1 - dar um jeito de pegar o elemento HTML dos botões
+const botoesCarrossel = document.querySelectorAll('.botao');
+const imagens = document.querySelectorAll('.imagem');
+
+// passo 2 - dar um jeito de identificar o clique do usuário no botão
+botoesCarrossel.forEach((botao, indice) => {
+    botao.addEventListener('click', () => {
+        // passo 3 - desmarcar o botão selecionado anterior      
+        desativarBotaoSelecionado();
+
+        // passo 4 - marcar o botão clicado como se estivesse selecionado
+        selecionarBotaoCarrosel(botao);
+
+        // passo 5 - esconder a imagem ativa de fundo
+        esconderImagemAtiva();
+
+        // passo 6 - fazer aparecer a imagem de fundo correspondente ao botão clicado
+        mostrarImagemDeFundo(indice);
+    })
+})
+
+function mostrarImagemDeFundo(indice) {
+    imagens[indice].classList.add('ativa');
 }
 
-botaoTrailer.addEventListener("click", () => {
-    alternarModal();
-video.setAttribute("src", linkDoVideo);
-});
+function selecionarBotaoCarrosel(botao) {
+    botao.classList.add('selecionado');
+}
 
-botaoFecharModal.addEventListener("click", () => {
-    alternarModal();
-    video.setAttribute("src", "");
-});
+function esconderImagemAtiva() {
+    const imagemAtiva = document.querySelector('.ativa');
+    imagemAtiva.classList.remove('ativa');
+}
+
+function desativarBotaoSelecionado() {
+    const botaoSelecionado = document.querySelector('.selecionado');
+    botaoSelecionado.classList.remove('selecionado');
+}
